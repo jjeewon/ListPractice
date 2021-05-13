c934b31ee5a7e613a1fc666714c8de1b8a45c4d1
jjeewon <wldnjs594@gmail.com>
2021-05-13 15:55:53 +0900

    Company Entity 정의

---
+++ app/src/main/java/com/example/listpractice/data/entity/Company.java
@@ -0,0 +1,19 @@
+package com.example.listpractice.data.entity;
+
+public class Company {
+    private String name;
+    private String catchPhrase;
+    private String bs;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBs() {
+        return bs;
+    }
+
+    public String getCatchPhrase() {
+        return catchPhrase;
+    }
+}
