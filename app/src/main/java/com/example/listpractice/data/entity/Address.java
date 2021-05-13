546ac98402a5028341363bd932c1e1cceea7b430
jjeewon <wldnjs594@gmail.com>
2021-05-13 15:56:46 +0900

    Address Entity 정의

---
+++ app/src/main/java/com/example/listpractice/data/entity/Address.java
@@ -0,0 +1,24 @@
+package com.example.listpractice.data.entity;
+
+public class Address {
+    private String street;
+    private String suite;
+    private String city;
+    private String zipcode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+}
