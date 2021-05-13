6df6ed072c2854cde1103a91e4aad2b7177002ab
jjeewon <wldnjs594@gmail.com>
2021-05-13 15:49:06 +0900

    Post Entity 정의
    Post 필드
    1. userId : long
    2. id : long
    3. title : String
    4. body : String

--- app/src/main/java/com/example/listpractice/data/entity/Post.java
+++ app/src/main/java/com/example/listpractice/data/entity/Post.java
@@ -4,6 +4,50 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Post implements Parcelable {
+    private long userId;
+    private long id;
+    private String title;
+    private String body;
+
+    public Post(long userId, long id, String title, String body){
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    protected Post(Parcel in) {
+        userId = in.readLong();
+        id = in.readLong();
+        title = in.readString();
+        body = in.readString();
+    }
+
+    public long getUserId(){
+        return userId;
+    }
+    public long getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getBody(){
+        return body;
+    }
+
+    public static final Creator<Post> CREATOR = new Creator<Post>() {
+        @Override
+        public Post createFromParcel(Parcel source) {
+            return new Post(source);
+        }
+
+        @Override
+        public Post[] newArray(int size) {
+            return new Post[size];
+        }
+    };
+
     @Override
     public int describeContents() {
         return 0;
@@ -11,6 +55,9 @@ public class Post implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-
+        dest.writeLong(userId);
+        dest.writeLong(id);
+        dest.writeString(title);
+        dest.writeString(body);
     }
 }
