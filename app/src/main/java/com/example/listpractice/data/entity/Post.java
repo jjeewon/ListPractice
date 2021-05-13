1f20205923487d6b07f540ccae7c0e303b98d204
jjeewon <wldnjs594@gmail.com>
2021-05-13 15:41:49 +0900

    게시글 엔터티 정의
    Post : 게시글 entity
    extends Parcelable 하므로,
    describeContents + writeToParcel
    오버라이딩함

---
+++ app/src/main/java/com/example/listpractice/data/entity/Post.java
@@ -0,0 +1,16 @@
+package com.example.listpractice.data.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Post implements Parcelable {
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
