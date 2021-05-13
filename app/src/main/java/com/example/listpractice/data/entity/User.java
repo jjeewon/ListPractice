0b11a659cafcc7c4f312652fd93843283ac8b929
jjeewon <wldnjs594@gmail.com>
2021-05-13 15:55:00 +0900

    User Entity 정의

---
+++ app/src/main/java/com/example/listpractice/data/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.listpractice.data.entity;
+
+public class User {
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private String phone;
+    private String website;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+}
