4df2287915afd8616d2ea5c7dce1f8ab527dee90
jjeewon <wldnjs594@gmail.com>
2021-05-14 14:08:56 +0900

    fragment_post 생성

--- app/src/main/java/com/example/listpractice/ui/post/PostViewModel.java
+++ app/src/main/java/com/example/listpractice/ui/post/PostViewModel.java
@@ -15,4 +15,12 @@ public class PostViewModel extends AndroidViewModel {
     public PostViewModel(@NonNull Application application){
         super(application);
     }
+
+    public void loadPosts(){
+
+    }
+
+    public MutableLiveData<Boolean> getLoading() {
+        return loading;
+    }
 }
