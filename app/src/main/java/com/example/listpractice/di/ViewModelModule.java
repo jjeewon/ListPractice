f9ceb2d69979311afddd840a23afd0f55924797c
jjeewon <wldnjs594@gmail.com>
2021-05-14 17:17:22 +0900

    PostModule 정의
    PostFragment에 멤버인젝션의 필요 모듈 정의

--- app/src/main/java/com/example/listpractice/di/ViewModelModule.java
+++ app/src/main/java/com/example/listpractice/di/ViewModelModule.java
@@ -1,9 +1,13 @@
 package com.example.listpractice.di;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.listpractice.ui.post.PostViewModel;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
 
 // ViewModel과 관련된 내용을 오브젝트 그래프로 관리
 //ViewModel과 관련된 내용을 오브젝트 그래프로 관리
@@ -13,5 +17,9 @@ public abstract class ViewModelModule {
     @Binds
     abstract ViewModelProvider.Factory bindViewModelFactory(AppViewModelFactory factory);
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(PostViewModel.class)
+    abstract ViewModel bindsPostViewModel(PostViewModel viewModel);
 
 }
