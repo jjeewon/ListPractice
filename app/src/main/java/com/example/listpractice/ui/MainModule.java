ee884dd6c1e8460bbbb899559774fc73ca222ecd
jjeewon <wldnjs594@gmail.com>
2021-05-14 17:21:12 +0900

    PostModule을 MainModule의 서브컴포넌트로 정의

--- app/src/main/java/com/example/listpractice/ui/MainModule.java
+++ app/src/main/java/com/example/listpractice/ui/MainModule.java
@@ -8,9 +8,13 @@ import com.example.listpractice.R;
 import com.example.listpractice.databinding.ActivityMainBinding;
 import com.example.listpractice.di.ActivityContext;
 import com.example.listpractice.di.ActivityScope;
+import com.example.listpractice.di.FragmentScope;
+import com.example.listpractice.ui.post.PostFragment;
+import com.example.listpractice.ui.post.PostModule;
 
 import dagger.Module;
 import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class MainModule {
@@ -25,4 +29,11 @@ public abstract class MainModule {
     static Context provideContext(MainActivity activity) {
         return activity;
     }
+
+    /**
+     * 서브 컴포넌트 정의
+     */
+    @FragmentScope
+    @ContributesAndroidInjector(modules = PostModule.class)
+    abstract PostFragment getPostFragment();
 }
