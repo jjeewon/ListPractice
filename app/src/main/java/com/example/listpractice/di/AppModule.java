5aeec09c50ddf757a7984b9bedc18ba51c40935d
jjeewon <wldnjs594@gmail.com>
2021-05-14 12:57:33 +0900

    Retrofit 객체 생성
    Retrofit으로 서버에서 데이터 가져온다.
    Retrofit 객체 - Gson 컨버터 + Rx 어댑터를 적용하여 생성

--- app/src/main/java/com/example/listpractice/di/AppModule.java
+++ app/src/main/java/com/example/listpractice/di/AppModule.java
@@ -11,7 +11,9 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module(includes = ViewModelModule.class)
 public class AppModule {
@@ -37,6 +39,14 @@ public class AppModule {
         return new SingleLiveEvent<>();
     }
 
- 
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(){
+        return new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+    }
 
 }
