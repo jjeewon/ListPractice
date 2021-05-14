331ef28cebfc9afd5df39fc64e0c70852e671cd1
jjeewon <wldnjs594@gmail.com>
2021-05-14 14:15:03 +0900

    PostFragment 구현
    - DaggerFragment 상속받아서 멤버 인젝션 구현함
    - 서버에 게시 글(post) 데이터 요청하고 응답을 받아 RecyclerView로 글 목록 표현함

--- app/src/main/java/com/example/listpractice/ui/post/PostFragment.java
+++ app/src/main/java/com/example/listpractice/ui/post/PostFragment.java
@@ -1,5 +1,19 @@
 package com.example.listpractice.ui.post;
 
+import android.app.FragmentBreadCrumbs;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.listpractice.databinding.FragmentPostBinding;
+import com.example.listpractice.di.AppViewModelFactory;
+
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
@@ -13,6 +27,29 @@ public class PostFragment extends DaggerFragment {
     /**
      * 오브젝트 그래프로부터 멤버 인젝션
      */
+    @Inject
+    FragmentPostBinding binding;
+    @Inject
+    AppViewModelFactory viewModelFactory;
+
+    PostViewModel viewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // ViewModel 객체 요청
+        viewModel = new ViewModelProvider(this, viewModelFactory).get(PostViewModel.class);
+        if(savedInstanceState == null){
+            // 데이터 요청, 프래그먼트가 재생성되었을 떄는 요청하지 않는다.
+            viewModel.loadPosts();
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater layoutInflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState){
+        return binding.getRoot();   
+    }
 
 
 }
